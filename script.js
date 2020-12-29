8d7031f5f1b876723715b02e774464c21e5b3963
marleigh <volcana@gmail.com>
2020-12-28 19:45:22 -0800

    more entries

--- script.js
+++ script.js
@@ -1,32 +1,59 @@
 const flavorMax = 2; //can probably get this automatically eventually?
 
 const resultDisplay = document.querySelector(".results");
+const desc = document.querySelector(".desc");
+const btn = document.getElementById('btn');
+const resetBtn = document.getElementById('reset');
 
-document.getElementById('btn').addEventListener('click', generateRaT);
+let reset = () => location.reload();
+
+btn.addEventListener('click', generateRaT);
+resetBtn.addEventListener('click', reset);
 
 async function generateRaT() {
+
+    //hide the description and buttons
+    desc.classList.add('hide');
+    btn.classList.add('hide');
+    resetBtn.classList.remove('hide');
+
     const promise = await fetch('local.json');
     const rats = await promise.json();
 
+    //variable for the number of available flavors
+    const numberOfFlavors = rats.length;
+
     //generate random number for the flavor
-    let flavorNum = randomNum(0, 1);
+    let randomFlavor = randomNum(0, numberOfFlavors);
     
     //generate random number for the prompt
-    let promptNum = randomNum(0, 29);
+    let randomPrompt = randomNum(0, 29);
 
-    //pull the correct ones from json. 
+    let flavor = rats[randomFlavor].flavor;
+    let prompt = rats[randomFlavor].prompts[randomPrompt];
+    let promptNumber = randomPrompt + 1;
+    let theme = rats[randomFlavor].theme;
+    let color = rats[randomFlavor].color;
 
+    //use provided color in styling the result!
 
-    // let array = rats[0].prompts[0];
-    // console.log(array);
-    let flavor = rats[flavorNum].flavor;
-    let prompt = rats[flavorNum].prompts[promptNum];
-    console.log(flavor);
-
-    //add to html. append to results
     let div = document.createElement('div');
-    div.innerText = `Flavor is: ${flavor} and your prompt is: ${prompt}`;
+
+    div.classList.add('test');
+    div.style.color = color;
+
+    div.innerText = `Flavor is: ${flavor} (${theme}) and your prompt is: ${promptNumber}: ${prompt}`;
     resultDisplay.appendChild(div);
 }
 
 let randomNum = (min, max) =>  random = Math.floor((Math.random() * (max - min + 1)) + min); 
+
+//json template
+// {
+//     "flavor": "",
+//     "theme": "",
+//     "color": "",
+//     "prompts": [
+        
+//     ]
+// }
