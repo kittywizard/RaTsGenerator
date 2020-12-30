a8438d3b2a52c00e26c42d4dc6fe7d70bca91919
marleigh <volcana@gmail.com>
2020-12-29 16:57:05 -0800

    JS updates. dynamic list of prompts

--- script.js
+++ script.js
@@ -4,24 +4,25 @@ const resultDisplay = document.querySelector(".results");
 const desc = document.querySelector(".desc");
 const btn = document.getElementById('btn');
 const resetBtn = document.getElementById('reset');
+const list = document.getElementById('pick-a-rat');
 
-let reset = () => location.reload();
+//let reset = () => location.reload(); //maybe not just reset but just run the generate function again?
 
 btn.addEventListener('click', generateRaT);
-resetBtn.addEventListener('click', reset);
+//resetBtn.addEventListener('click', reset);
 
 async function generateRaT() {
 
     //hide the description and buttons
-    desc.classList.add('hide');
-    btn.classList.add('hide');
-    resetBtn.classList.remove('hide');
+    //desc.classList.add('hide');
+    //btn.classList.add('hide');
+    //resetBtn.classList.remove('hide');
 
     const promise = await fetch('local.json');
     const rats = await promise.json();
 
     //variable for the number of available flavors
-    const numberOfFlavors = rats.length;
+    const numberOfFlavors = rats.length - 1;
 
     //generate random number for the flavor
     let randomFlavor = randomNum(0, numberOfFlavors);
@@ -35,16 +36,58 @@ async function generateRaT() {
     let theme = rats[randomFlavor].theme;
     let color = rats[randomFlavor].color;
 
-    //use provided color in styling the result!
+    //creating html elements for display
+    let flavorDiv = document.createElement('div');
+    flavorDiv.classList.add('flavor');
+    //flavorDiv.style.color = color;
+    flavorDiv.innerText = `Flavor: ${flavor}`;
+    resultDisplay.appendChild(flavorDiv);
 
-    let div = document.createElement('div');
+    let promptDiv = document.createElement('div');
+    promptDiv.classList.add('prompt');
+    //promptDiv.style.color = color;
+    promptDiv.innerText = `#${promptNumber}: ${prompt}`;
 
-    div.classList.add('test');
-    div.style.color = color;
-
-    div.innerText = `Flavor is: ${flavor} (${theme}) and your prompt is: ${promptNumber}: ${prompt}`;
-    resultDisplay.appendChild(div);
+    flavorDiv.appendChild(promptDiv);
 }
+
+function getList(){
+    fetch('local.json')
+        .then(response => response.json())
+        .then(text => {
+            //grab the first group
+            let copy = text[0].group;
+
+            // let option = document.createElement('div');
+            // option.innerText = copy;
+            // document.body.appendChild(option);
+
+            let groupArray = [];
+
+            //run through the entire array
+            text.forEach(flavor => {
+                groupArray.push(flavor.group);
+                console.log(groupArray);
+
+                //so we've got an array with everything in it. 
+
+                // groupArray.forEach(element => {
+                //     if(element === copy) {
+                //         //this means that the current copy is the same as the array. keep going
+                //     } else {
+                //         copy = flavor.group;
+                //         console.log(`False, Copy is now: ${copy}`);
+
+                //         let option = document.createElement('div');
+                //         option.innerText = copy;
+                //         document.body.appendChild(option);
+                //     }
+                // });
+            });
+        });
+}
+
+getList();
 
 let randomNum = (min, max) =>  random = Math.floor((Math.random() * (max - min + 1)) + min); 
 
