6578563b567a7fefbd616fb669729c8da5d175ea
marleigh <volcana@gmail.com>
2022-04-23 15:15:01 -0700

    manually importing icons

--- runaway-tales/src/components/Header.js
+++ runaway-tales/src/components/Header.js
@@ -2,7 +2,7 @@ import Nav from "./Nav";
 
 export default function Header(){
     return (
-        <header className="p-6 mx-auto container flex items-center justify-between bg-gray-green-light">
+        <header className="p-6 mx-auto container flex items-center justify-between bg-gray-green-light rounded-b-lg">
             <div className="container">
                <h1 className="text-dark-green text-4xl font-bold font-serif">
                      Runaway Tales
