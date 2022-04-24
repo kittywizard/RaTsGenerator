a99170728a5fdb50b2ac3d83747895b1cac9d80e
marleigh <volcana@gmail.com>
2022-04-24 10:43:00 -0700

    creating custom hook

--- runaway-tales/src/Main.js
+++ runaway-tales/src/Main.js
@@ -1,38 +1,13 @@
-import { useEffect, useState } from "react"
 import Button from "./components/Button";
 import Prompt from "./components/Prompt";
-import { flavorData } from "./data/flavors";
 import { toppings } from "./data/toppings";
 import {nanoid } from "nanoid";
 
-//import useGenerator from "./hooks/useGenerator"
+import useGenerator from "./hooks/useGenerator"
 
 export default function Main() {
 
-    const [flavors, setFlavors] = useState(flavorData);
-    const [chosenPrompts, setChosenPrompts] = useState([]);
-
-    function getPrompt() {
-        //generate random number
-        let randomFlavorNum = Math.floor((Math.random() * (flavors.length - 0 + 1)) + 0);
-        let randomPromptNum = Math.floor((Math.random() * (30 - 0 + 1)) + 0);
-
-        let prompt = flavors[randomFlavorNum].prompts[randomPromptNum];
-
-        let promptObj = {
-            prompt: prompt,
-            flavor: flavors[randomFlavorNum].flavor,
-            number: randomPromptNum,
-            promptType: flavors[randomFlavorNum].theme
-        }
-
-        setChosenPrompts(prevState => {
-            return [
-                ...prevState,
-                promptObj
-            ]
-        });
-    }
+    const {getPrompt, chosenPrompts} = useGenerator();
 
     const promptMap = chosenPrompts.map(prompt => (
         <Prompt 
--- runaway-tales/src/hooks/useGenerator.js
+++ runaway-tales/src/hooks/useGenerator.js
@@ -1,10 +1,35 @@
 import {useState, useEffect} from "react";
+import { flavorData } from "../data/flavors";
+
 
 function useGenerator() {
-    function generatePrompt() {
-        //need to call flavors
+
+    const [flavors, setFlavors] = useState(flavorData);
+    const [chosenPrompts, setChosenPrompts] = useState([]);
+
+    function getPrompt() {
+        //generate random number
+        let randomFlavorNum = Math.floor((Math.random() * (flavors.length - 0 + 1)) + 0);
+        let randomPromptNum = Math.floor((Math.random() * (30 - 0 + 1)) + 0);
+
+        let prompt = flavors[randomFlavorNum].prompts[randomPromptNum];
+
+        let promptObj = {
+            prompt: prompt,
+            flavor: flavors[randomFlavorNum].flavor,
+            number: randomPromptNum,
+            promptType: flavors[randomFlavorNum].theme
+        }
+
+        setChosenPrompts(prevState => {
+            return [
+                ...prevState,
+                promptObj
+            ]
+        });
     }
-    return []
+
+    return {getPrompt, flavors, chosenPrompts, setChosenPrompts}
 }
 
 export default useGenerator;
